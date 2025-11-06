9d48871957cd7657f474fbfc0c2581b14350518e
elenalazo20 <139652373+elenalazo20@users.noreply.github.com>
2025-11-06 17:16:55 -0300

    feat: configuración inicial (here/fs)
    crea estructura y árbol de carpetas

---
+++ R/01_configuracion.R
@@ -0,0 +1,23 @@
+# R/01_configuracion.R ----------------------------------------------------
+suppressPackageStartupMessages({
+  library(here); library(fs); library(tidyverse)})
+
+#  Fijar raíz del proyecto ------------------------------------------------
+here::i_am("R/01_configuracion.R")
+cat("Raíz del proyecto:", here::here(), "\n")
+
+# 1) Carpetas base -----------------------------------------------------------
+dirs <- c("datos_crudos","datos_procesados","figs","output","reports")
+dir_create(here(dirs))
+
+# 2) Helper para guardar figuras con carpeta asegurada -----------------------
+guardar_fig <- function(p, file, w=10, h=6, dpi=300){
+  dir_create(path_dir(file))
+  ggplot2::ggsave(file, p, width = w, height = h, dpi = dpi)}
+
+# 3) Gráfico del árbol inicial de carpetas con fs::dir_tree() -------------
+tree <- capture.output(dir_tree(here(), recurse = TRUE))
+writeLines(tree, here("output/estructura_proyecto.txt"))
+writeLines(c("```", tree, "```"), here("output/estructura_proyecto.md"))
+
+message("Estructura creada ✅ y árbol guardado en output/estructura_proyecto.{txt,md}")
